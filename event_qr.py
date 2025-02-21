648d25da8b4101922146542b9dc0616388f12817
Prince Raj <168627808+Geet-Prince@users.noreply.github.com>
2025-02-21 22:33:46 +0530

    Update event_qr.py

--- event_qr.py
+++ event_qr.py
@@ -9,8 +9,8 @@ from io import BytesIO
 from PIL import Image, ImageDraw, ImageFont
 
 # --- Authentication Setup ---
-USERNAME = "admin"  # Change this to your preferred username
-PASSWORD = "password123"  # Change this to your preferred password
+USERNAME = "prince"  # Change this to your preferred username
+PASSWORD = "papa hai mere"  # Change this to your preferred password
 
 def login():
     """Displays the login form."""
